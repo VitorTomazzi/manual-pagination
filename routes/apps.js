88cd989cf442bad19beff472ab8d4dabe14650de
Vitor Tomazzi <vitor.c.tomazzi@gmail.com>
2019-11-03 17:13:36 -0500

    query added

--- routes/apps.js
+++ routes/apps.js
@@ -1,43 +1,45 @@
-const express = require('express');
-const router = express.Router();
-// const data = [
-//     {id: 1, name: 'my-app-001},
-//     {id: 2, name: 'my-app-002},
-//     {id: 3, name: 'my-app-003},
-//     {id: 4, name: 'my-app-004},
-//     {id: 5, name: 'my-app-005},
-//     {id: 6, name: 'my-app-006},
-//     {id: 7, name: 'my-app-007},
-//     {id: 8, name: 'my-app-008},
-//     {id: 9, name: 'my-app-009},
-//     {id: 10, name: 'my-app-010}
-// ];
+// const express = require('express');
+// const router = express.Router();
 
-// instead of manually creating data, we loop and push into empty data array
-let data = [];
-for (let i = 1; i <= 300; i++) {
-	data.push({
-		id: Number(i),
-		name: `my-app-${i}`
-	});
-}
+//********** scrapped this solution/ no longer applicable ************
 
-// router logic to print the data onto our web app. this GET route prints every app
-router.get('/', (req, res, next) => {
-	res.status(200).send(data);
-});
+// // const data = [
+// //     {id: 1, name: 'my-app-001},
+// //     {id: 2, name: 'my-app-002},
+// //     {id: 3, name: 'my-app-003},
+// //     {id: 4, name: 'my-app-004},
+// //     {id: 5, name: 'my-app-005},
+// //     {id: 6, name: 'my-app-006},
+// //     {id: 7, name: 'my-app-007},
+// //     {id: 8, name: 'my-app-008},
+// //     {id: 9, name: 'my-app-009},
+// //     {id: 10, name: 'my-app-010}
+// // ];
 
-// this GET route lets us print each app by id
-router.get('/:id', (req, res, next) => {
-	let id = req.params.id;
+// // instead of manually creating data, we loop and push into empty data array
+// let data = [];
+// for (let i = 1; i <= 300; i++) {
+// 	data.push({
+// 		id: Number(i),
+// 		name: `my-app-${i}`
+// 	});
+// }
 
-	let found = data.find((item) => {
-		return item.id === parseInt(id);
-	});
-	if (found) {
-		res.status(200).send(found);
-	} else {
-		res.sendStatus(404);
-	}
-});
-module.exports = router;
+// // router logic to print the data onto our web app. this GET route prints every app
+// // router.get('/', (req, res, next) => {
+// // 	res.status(200).json('Backend Pagination');
+// // });
+
+// // this GET route lets us print each app by id
+// router.get('/apps/:id', (req, res, next) => {
+
+// 	// let found = data.find((item) => {
+// 	// 	return item.id === parseInt(req.params.id);
+// 	// });
+// 	// if (found) {
+// 	// 	res.status(200).json(found);
+// 	// } else {
+// 	// 	res.sendStatus(404);
+// 	// }
+// });
+// module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,68 @@
 const http = require('http');
 const express = require('express');
-const appsRouter = require('./routes/apps');
 const app = express();
 
-app.use(express.json());
+// const data = [
+//     {id: 1, name: 'my-app-001},
+//     {id: 2, name: 'my-app-002},
+//     {id: 3, name: 'my-app-003},
+//     {id: 4, name: 'my-app-004},
+//     {id: 5, name: 'my-app-005},
+//     {id: 6, name: 'my-app-006},
+//     {id: 7, name: 'my-app-007},
+//     {id: 8, name: 'my-app-008},
+//     {id: 9, name: 'my-app-009},
+//     {id: 10, name: 'my-app-010}
+// ];
 
-app.use('/apps', appsRouter);
+//loop to create as much filler data as we want
+let data = [];
+for (let i = 1; i <= 300; i++) {
+	data.push({
+		id: Number(i),
+		name: `my-app-${i}`
+	});
+}
 
-//checked connected
-app.use('/', (req, res) => {
-	res.send('Hello World');
+//checks if connected
+app.get('/', (req, res) => {
+	res.status(200).send('Backend Pagination');
+});
+
+// this GET route lets us print each app that we want to query(?) using page= & limit=
+app.get('/apps', (req, res) => {
+	const page = parseInt(req.query.page); //parse int to make queries return as numbers and not strings
+	const limit = parseInt(req.query.limit);
+
+	//page * limit will give us which slice of the data we want to look at
+	const startIndex = (page - 1) * limit; //page - 1 because we start a 0 index
+	const endIndex = page * limit;
+
+	const range = {};
+	// if (req.params === null) {
+	// 	range = { data };
+	// } else {
+	// 	range = {};
+	// }
+
+	//checks if surrounding pages exist
+	if (endIndex < data.length) {
+		range.next = {
+			page: page + 1,
+			limit: limit
+		};
+	}
+
+	if (startIndex > 0) {
+		range.previous = {
+			page: page - 1,
+			limit: limit
+		};
+	}
+
+	range.range = data.slice(startIndex, endIndex);
+
+	res.json(range);
 });
 
 const server = http.createServer(app);
