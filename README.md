b31acbe950afbb449d90c3ab4dfa41e6cbc2f549
Vitor Tomazzi <vitor.c.tomazzi@gmail.com>
2019-11-05 11:27:53 -0500

    commentary

--- README.md
+++ README.md
@@ -1,4 +1,72 @@
 # Manual Pagination
 
-A single endpoint app built using Express and Node without the use of external libraries. 
-First, I created dummy data using a for loop. Using queries added onto the "/apps" endpoint (?page= # & limit= #) allows us to specify which page we would like to view while limiting the number of data entries printed per page. The query parameter (req.query) is used over the path parameter (req.param) as it is the best practice when sorting or filtering data. If no page is specified, the default page is 1. If no limit is set, the app limits itself to 50 entries per page. A check is also implemented to show the previous and next pages available and how many entries will be shown.
+<div align=center>
+    <h1 align=center>
+        <br>
+        Manual Pagination Challenge
+        <br>
+        <img width="100px" align=center src="https://nodejs.org/static/images/logos/nodejs-new-pantone-black.png" alt="NodeJS logo">
+    </h1>
+</div>
+
+## 📝 Table of Contents
+- [About](#about)
+- [Setup](#setup)
+- [Endpoints](#endpoints)
+- [Built Using](#built_using)
+- [Authors](#authors)
+
+## 📙 About <a name = "about"></a>
+
+A single endpoint app built using the Express framework and the Node environment without the use of external libraries.
+
+#### Process
+1. Set up Express and Node environment with server listening on port 3000
+2. Installed necessary dependencies in package.json
+3. Created dummy data using loops
+4. Created endpoint, added manual pagination, and incorporated sorting functionality
+5. Deployed to Heroku
+
+
+### Queries and Pagination
+Using queries added onto the "/apps" endpoint allows us to specify which page we would like to view while limiting the number of data entries printed per page. The query parameter (req.query) is used over the path parameter (req.param) as it is the best practice when sorting or filtering data. If no queries are specified in the URI, built-in default values keeps everything running smoothly. A check is also implemented to show the previous and next pages available and how many entries will be shown on those pages.
+
+
+## ⚙️ Setup <a name = "setup"></a>
+
+### Installation
+
+*   Clone the [repo]('https://github.com/VitorTomazzi/manual-pagination/')
+*   Install the server dependencies with: ```npm install``` or ```yarn install```
+*   Run the local backend server using ```npm start``` or ```yarn start```
+*   Open ```http://localhost:3000``` and start entering your test queries into the URI
+
+
+## 📡 Endpoints <a name = "endpoints"></a>
+### <span>**GET**</span> /apps
+
+| Example Queries |
+| --- |
+| https://manual-pagination.herokuapp.com/apps |
+| https://manual-pagination.herokuapp.com/apps?page=1 |
+| https://manual-pagination.herokuapp.com/apps?page=2&limit=5 |
+| https://manual-pagination.herokuapp.com/apps?page=1&sortBy=name&orderBy=desc |
+| https://manual-pagination.herokuapp.com/apps?page=2&limit=3&sortBy=name&orderBy=desc |
+
+
+| Query Parameters | Type | Default | Description |
+|:--- | :--- | :--- | :--- |
+| page | ```Number``` | ```1``` |Paginates to the requested page. |
+| limit | ```Number``` | ```50``` |Limits number of entries per page. |
+| sortBy | ```String``` | ```"id"``` | Only accepts "id" or "name". |
+| orderBy | ```String``` | ```"asc"``` | Enter 'asc' for ascending order or 'desc' for descending order. |
+
+
+## ⛏️ Built Using <a name = "built_using"></a>
+- [JavaScript](https://www.javascript.com/) - Front End
+- [Express.js](https://expressjs.com/) - Backend
+- [Node.js](https://nodejs.org/) - Backend
+
+
+# ✍️ Authors <a name = "authors"></a>
+* [Vitor Tomazzi](https://github.com/VitorTomazzi)
--- server.js
+++ server.js
@@ -2,39 +2,50 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
-// const data = [
-// 	{ id: 1, name: 'ff-app-001' },
-// 	{ id: 2, name: 'bb-app-002' },
-// 	{ id: 3, name: 'qq-app-003' },
-// 	{ id: 4, name: 'dd-app-004' },
-// 	{ id: 5, name: 'ee-app-005' },
-// 	{ id: 6, name: 'aa-app-006' },
-// 	{ id: 7, name: 'my-app-007' },
-// 	{ id: 8, name: 'my-app-008' },
-// 	{ id: 9, name: 'zz-app-009' },
-// 	{ id: 10, name: 'my-app-010' }
-// ];
+//this dummy data is better for testing
+const data = [
+	{ id: 1, name: 'ff-app-001' },
+	{ id: 2, name: 'bb-app-002' },
+	{ id: 3, name: 'qq-app-003' },
+	{ id: 4, name: 'dd-app-004' },
+	{ id: 5, name: 'ee-app-005' },
+	{ id: 6, name: 'aa-app-006' },
+	{ id: 7, name: 'my-app-007' },
+	{ id: 8, name: 'ay-app-008' },
+	{ id: 9, name: 'zz-app-009' },
+	{ id: 10, name: 'my-app-010' },
+	{ id: 11, name: 'ff-app-001' },
+	{ id: 12, name: 'zb-app-002' },
+	{ id: 13, name: 'qr-app-003' },
+	{ id: 14, name: 'sd-app-004' },
+	{ id: 15, name: 'ee-app-005' },
+	{ id: 16, name: 'rr-app-006' },
+	{ id: 17, name: 'uy-app-007' },
+	{ id: 18, name: 'uu-app-008' },
+	{ id: 19, name: 'lo-app-009' },
+	{ id: 20, name: 'my-app-010' }
+];
 
-// loop to create as much filler data as we want
-let data = [];
-for (let i = 1; i <= 300; i++) {
-	data.push({
-		id: Number(i),
-		name: `my-app-${i}`
-	});
-}
+// can also use a loop to create a larger volume of filler data
+// let data = [];
+// for (let i = 1; i <= 300; i++) {
+// 	data.push({
+// 		id: Number(i),
+// 		name: `my-app-${i}`
+// 	});
+// }
 
 //checks if connected
 app.get('/', (req, res) => {
 	res.status(200).send('Backend Pagination');
 });
 
-// this GET route lets us print each app that we want to query(?) using page= & limit=
+// this GET route lets us print each app that we want to query
 app.get('/apps', (req, res) => {
-	const page = parseInt(req.query.page) || 1; //parse int to make queries return as numbers and not strings. default page is 1 if not specified
+	const page = parseInt(req.query.page) || 1; //default page is 1 if not specified
 	const limit = parseInt(req.query.limit) || 50; //limits entries per page. default is 50 if not specified
-	const sortBy = req.query.sortBy; //name or id
-	const orderBy = req.query.orderBy; //asc or desc
+	const sortBy = req.query.sortBy || 'id'; //sort by name or id. defaults to sorting by id
+	const orderBy = req.query.orderBy || 'asc'; //order by asc or desc. defaults ascending
 
 	//page * limit will give us which slice of the data we want to look at
 	const startIndex = (page - 1) * limit; //page - 1 because we start a 0 index
@@ -57,26 +68,39 @@ app.get('/apps', (req, res) => {
 		};
 	}
 
-	// orderBy id or name and sortBy asc or desc
-	// if (req.query.sortBy && req.query.orderBy) {
-	// 	results[req.query.sortBy] = req.query.orderBy === 'desc' ? -1 : 1;
-	// }
-
-	// if sortBy is name and orderBy is asc then ...
-
-	// if sortBy is id and orderBy is asc then ...
-
-	// sort either asc or desc
-	function order(data, orderBy) {
-		return data.sort((a, b) => {
-			return orderBy == 'asc' ? a.id - b.id : b.id - a.id;
-		});
+	// logic for sorting either asc or desc && by name or id when query is specified
+	function order(data, sortBy, orderBy) {
+		if (sortBy === 'id') {
+			return data.sort((a, b) => {
+				return orderBy == 'asc' ? a.id - b.id : b.id - a.id;
+			});
+		} else if (sortBy === 'name') {
+			return orderBy == 'asc'
+				? data.sort((a, b) => {
+						// console.log(a, b);
+						//used regex for practice. alternatively can use String.localCompare method
+						//i.e. needed to correctly order 'my-app-10' > 'my-app-2'
+						//usage depends on the data set being used
+						if (a.name.match(/[^0-9]+/gi)[0] === b.name.match(/[^0-9]+/gi)[0]) {
+							return a.name.match(/[0-9]+/)[0] - b.name.match(/[0-9]+/)[0];
+						}
+						return a.name > b.name ? 1 : -1;
+					})
+				: data.sort((a, b) => {
+						if (a.name.match(/[^0-9]+/gi)[0] === b.name.match(/[^0-9]+/gi)[0]) {
+							return b.name.match(/[0-9]+/)[0] - a.name.match(/[0-9]+/)[0];
+						}
+						return a.name < b.name ? 1 : -1;
+					});
+		} else {
+			return data;
+		}
 	}
 
-	results.results = order(data.slice(startIndex, endIndex), orderBy);
+	// if we paginate, sort, or order we will be passing those queries here into the order function
+	results.results = order(data.slice(startIndex, endIndex), sortBy, orderBy);
 
-	// console.log(results.results);
-
+	//print on screen
 	res.json(results);
 });
 
